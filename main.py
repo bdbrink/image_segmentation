48367ffd3de48644654cee8dca3bf137feabc937
bdbrink <brendan.brink@outlook.com>
2023-06-21 17:07:27 -0500

    added plots for segmented image

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ print(image.shape)
 x = image.reshape(-1, 3)
 print(x.shape)
 
+# find 2 clusters in image
 kmeans = KMeans(n_clusters=2,  n_init=10)
 kmeans.fit(x)
+
+segmented_img = kmeans.cluster_centers_[kmeans.labels_]
+segmented_img = segmented_img.reshape(image.shape)
+plt.imshow(segmented_img / 255)
