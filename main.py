c0e58259ad38ed78396bfe4a5c7aa8dad1543a93
bdbrink <brendan.brink@outlook.com>
2023-06-26 17:31:12 -0500

    comparing image compression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import cv2
 
 from sklearn.cluster import KMeans
 
@@ -12,10 +13,14 @@ print(image.shape)
 x = image.reshape(-1, 3)
 print(x.shape)
 
-# find 2 clusters in image
-kmeans = KMeans(n_clusters=2,  n_init=10)
+# More clusters, more detail
+# Performance is downgraded though
+kmeans = KMeans(n_clusters=10,  n_init=10)
 kmeans.fit(x)
 
 segmented_img = kmeans.cluster_centers_[kmeans.labels_]
 segmented_img = segmented_img.reshape(image.shape)
 plt.imshow(segmented_img / 255)
+
+cv2.imwrite("image1.png", cv2.cvtColor(image, cv2.COLOR_BAYER_BG2BGR))
+cv2.imwrite("image1.png", cv2.cvtColor(segmented_img.astype("uint8"), cv2.COLOR_BAYER_BG2BGR))
